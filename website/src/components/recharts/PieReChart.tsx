36b6b2fe35aa793862cacd67301b114ea6231c0d
Math <mathieu@lescaudron.com>
2024-04-25 20:52:10 +0200

    Update data, deploy:website

--- website/src/components/recharts/PieReChart.tsx
+++ website/src/components/recharts/PieReChart.tsx
@@ -18,6 +18,9 @@ export default function PieReChart({ ...props }) {
             );
           })}
         </Pie>
+
+        <Legend />
+        <Tooltip />
       </PieChart>
     </ResponsiveContainer>
   );
